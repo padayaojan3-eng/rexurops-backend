3f1caacc95356f98e39fc0519f9af167e43931ea
Your Name <your@email.com>
2026-05-20 20:39:26 +0800

    Fix: always set superuser password on deploy

--- accounts/management/commands/ensure_superuser.py
+++ accounts/management/commands/ensure_superuser.py
@@ -4,14 +4,10 @@ from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
-    help = 'Create a superuser from environment variables if none exists'
+    help = 'Create or update superuser from environment variables'
 
     def handle(self, *args, **options):
         User = get_user_model()
-        if User.objects.filter(is_superuser=True).exists():
-            self.stdout.write('Superuser already exists, skipping.')
-            return
-
         username = os.environ.get('SUPERUSER_USERNAME', 'admin')
         email = os.environ.get('SUPERUSER_EMAIL', '')
         password = os.environ.get('SUPERUSER_PASSWORD', '')
@@ -20,5 +16,14 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('SUPERUSER_PASSWORD not set, skipping.'))
             return
 
-        User.objects.create_superuser(username=username, email=email, password=password)
-        self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully.'))
+        user, created = User.objects.get_or_create(username=username)
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        if created:
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created.'))
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" password updated.'))
